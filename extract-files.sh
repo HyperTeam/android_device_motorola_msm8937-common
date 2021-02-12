a2e0bd978ff3492ff871d6783ee7600776f0da62
the-flash <flash@remainsilent.net>
2021-02-12 00:57:08 +0000

    msm8937-common: Drop camera hax
    Signed-off-by: the-flash <flash@remainsilent.net>

--- extract-files.sh
+++ extract-files.sh
@@ -69,10 +69,6 @@ function blob_fixup() {
         patchelf --set-soname keystore.msm8937.so "${2}"
         ;;
 
-    vendor/lib/libmmcamera2_sensor_modules.so)
-        sed -i 's|msm8953_mot_deen_camera.xml|msm8937_mot_camera_conf.xml|g' "${2}"
-        ;;
-
     vendor/lib/libjustshoot.so)
         patchelf --add-needed libjustshoot_shim.so "${2}"
         ;;
